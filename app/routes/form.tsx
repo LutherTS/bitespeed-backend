b20822d888f10aa9fa4ab5c7ba778f1527dfb64a
Luther Tchofo Safo <nintendobserver@gmail.com>
2024-06-07 19:03:54 +0200

    Trying out the reset route...

--- app/routes/form.tsx
+++ app/routes/form.tsx
@@ -1,10 +1,7 @@
-import { useFetcher } from "@remix-run/react";
 import Form from "~/components/form";
 import Reset from "~/components/reset";
 
 export default function FormPage() {
-  const fetcher = useFetcher();
-
   return (
     <>
       <Form />
